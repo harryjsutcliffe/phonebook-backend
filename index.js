254fa9c0a599b9d8d6a9f5b9843ee87d1737f2f3
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-12-31 19:45:50 +0900

    first commit for replit deployment

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+var morgan = require('morgan')
+morgan.token('data', function (req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :data'
+  )
+)
+
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<div>Phonebook has info for ${persons.length} people</div> 
+  <br>
+  ${Date()}`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(note => note.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  if (persons.map((person) => person.name).includes(person.name)) {
+    return response.status(400).json({ 
+      error: `${person.name} already exists in the phonebook`
+    })
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
