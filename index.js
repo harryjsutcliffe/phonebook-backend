bc0475e869266119de1af9dc77e29e9ac3db88c0
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-12-31 19:58:53 +0900

    add static content middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
+
 
 var morgan = require('morgan')
 morgan.token('data', function (req) {
